97e43429b7e903ba38d0219c1f3b180bf6106182
E R <postbox461@outlook.com>
2019-11-24 19:32:20 +0700

    Setup project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace Silver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloClick(object sender, RoutedEventArgs e)
+        {
+            // Title = "Hello, World";
+            // Title = System.Reflection.Assembly.GetEntryAssembly().Location;
+            // Title = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            Title = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            Title += Path.DirectorySeparatorChar.ToString();
+        }
+    }
+}
