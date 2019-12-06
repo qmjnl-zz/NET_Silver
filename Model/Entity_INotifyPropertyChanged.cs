8766d937012516d714ff2ead6e34e129daa9b0db
liveinmegacity <postbox461@outlook.com>
2019-12-06 10:01:03 +0700

    Refactor

---
+++ Model/Entity_INotifyPropertyChanged.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Silver
+{
+    public partial class Entity : INotifyPropertyChanged
+    {
+        private int id;
+        private bool isChanged;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (value == id) return;
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsChanged
+        {
+            get => isChanged;
+            set
+            {
+                if (value == isChanged) return;
+                isChanged = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName != nameof(IsChanged))
+            {
+                IsChanged = true;
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Model/Transaction.cs
+++ Model/Transaction.cs
@@ -1,25 +1,25 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+// using System.ComponentModel;
+// using System.Runtime.CompilerServices;
 
 namespace Silver
 {
-    public class Transaction : INotifyPropertyChanged
+    public class Transaction : Entity// INotifyPropertyChanged
     {
-        private int id;
+        // private int id;
         private decimal amount;
         private string comment;
-        private bool isChanged;
+        // private bool isChanged;
 
-        public int Id
-        {
-            get => id;
-            set
-            {
-                if (value == id) return;
-                id = value;
-                OnPropertyChanged();
-            }
-        }
+        // public int Id
+        // {
+        //     get => id;
+        //     set
+        //     {
+        //         if (value == id) return;
+        //         id = value;
+        //         OnPropertyChanged();
+        //     }
+        // }
         public decimal Amount
         {
             get => amount;
@@ -40,27 +40,27 @@ namespace Silver
                 OnPropertyChanged();
             }
         }
-        public bool IsChanged
-        {
-            get => isChanged;
-            set
-            {
-                if (value == isChanged) return;
-                isChanged = value;
-                OnPropertyChanged();
-            }
-        }
+        // public bool IsChanged
+        // {
+        //     get => isChanged;
+        //     set
+        //     {
+        //         if (value == isChanged) return;
+        //         isChanged = value;
+        //         OnPropertyChanged();
+        //     }
+        // }
 
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        // public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            if (propertyName != nameof(IsChanged))
-            {
-                IsChanged = true;
-            }
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        // protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        // {
+        //     if (propertyName != nameof(IsChanged))
+        //     {
+        //         IsChanged = true;
+        //     }
+        //     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        // }
     }
 }
