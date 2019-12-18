5653f9218ccc484247f3e7d0ce855d4839592523
liveinmegacity <postbox461@outlook.com>
2019-12-18 17:44:24 +0700

    Clean up code

--- Model/Repository.cs
+++ Model/Repository.cs
@@ -6,14 +6,6 @@ using LiteDB;
 
 namespace Silver
 {
-    // public class Customer
-    // {
-    //     public int Id { get; set; }
-    //     public string Name { get; set; }
-    //     public string[] Phones { get; set; }
-    //     public bool IsActive { get; set; }
-    // }
-
     public class Repository
     {
         private string filename = "data.silver";
@@ -34,31 +26,11 @@ namespace Silver
                 string TransactionType = typeof(Transaction).Name;
                 var col = db.GetCollection<Transaction>(TransactionType);
 
-                // Create your new customer instance
-                // var transaction = new Transaction { Amount = 120.50M, Comment = "Фрукты", IsChanged = false };
-
                 // Insert new customer document (Id will be auto-incremented)
                 col.Delete(x => true);
                 col.Insert(new Transaction { Amount = 120.50M, Comment = "Фрукты", IsChanged = false });
                 col.Insert(new Transaction { Amount = 70.45M, Comment = "Кофе", IsChanged = false });
                 col.Insert(new Transaction { Amount = 89.90M, Comment = "Печенье", IsChanged = false });
-
-                // Update a document inside a collection
-                // transaction.Name = "Joana Doe";
-                // col.Update(transaction);
-
-                // Index document using document Name property
-                // col.EnsureIndex(x => x.Name);
-
-                // Use LINQ to query documents
-                // var results = col.Find(x => x.Name.StartsWith("Jo"));
-
-                // Let's create an index in phone numbers (using expression). It's a multikey index
-                // col.EnsureIndex(x => x.Phones, "$.Phones[*]");
-
-                // and now we can query phones
-                // var r = col.FindOne(x => x.Phones.Contains("8888-5555"));
-                // var r = col.FindOne(x => (x.Phones as IList).Contains("8888-5555"));
             }
         }
 
